1f1766f97f1d6fb188a5ad471dfee4dd72486966
Abraham Romero <romero9804@hotmail.com>
2025-06-26 21:53:27 -0700

    feat: crud de entidad comercial

--- app/Http/Controllers/BusinessEntityController.php
+++ app/Http/Controllers/BusinessEntityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BusinessEntity;
 use Illuminate\Http\Request;
 
 class BusinessEntityController extends Controller
@@ -11,7 +12,8 @@ class BusinessEntityController extends Controller
      */
     public function index()
     {
-        //
+        $businessEntity = BusinessEntity::all();
+        return response()->json($businessEntity,200);
     }
 
     /**
@@ -19,7 +21,19 @@ class BusinessEntityController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            "name"=>"string|required|unique:business_entities",
+            "type"=>"string|required",
+            "rfc"=>"string|required",
+            "phone"=>"string|required",
+            "address"=>"string|required",
+            "email"=>"email|required",
+            "active"=>"boolean|required"
+        ]);
+
+        BusinessEntity::create($validated);
+
+        return response()->json(["message"=>"entidad comercial creada"],200);
     }
 
     /**
@@ -27,7 +41,8 @@ class BusinessEntityController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $businessEntities = BusinessEntity::findOrFail($id);
+        return response()->json($businessEntities);
     }
 
     /**
@@ -35,7 +50,21 @@ class BusinessEntityController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $businessEntity = BusinessEntity::findOrFail($id);
+
+        $validated = $request->validate([
+            "name"=>"string|required|unique:business_entities,name,{$id}",
+            "type"=>"string|required",
+            "rfc"=>"string|required",
+            "phone"=>"string|required",
+            "address"=>"string|required",
+            "email"=>"email|required",
+            "active"=>"boolean|required"
+        ]);
+
+        $businessEntity->update($validated);
+        return response()->json(['message'=>'entidad comercial actualizada']);
+
     }
 
     /**
@@ -43,6 +72,12 @@ class BusinessEntityController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $businessEntity = BusinessEntity::findOrFail($id);
+
+        $businessEntity->update([
+            'active' => false
+        ]);
+
+        return response()->json(['message'=>'entidad comercial desactivada'],200);
     }
 }
--- app/Models/BusinessEntity.php
+++ app/Models/BusinessEntity.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class BusinessEntity extends Model
-{
+{   
+    protected $fillable = ['name','type','rfc','phone','address','email','active'];
     public function contact()
     {
         return $this->hasMany(Contact::class);
